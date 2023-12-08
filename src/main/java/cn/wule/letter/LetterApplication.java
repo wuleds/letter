82b77aad5d0d6e45fb32c5b51d497eecf0bc1a7b
吴乐 <wl1030ds@gmail.com>
2023-12-08 23:57:59 +0800

    初始提交

---
+++ src/main/java/cn/wule/letter/LetterApplication.java
@@ -0,0 +1,13 @@
+package cn.wule.letter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LetterApplication.class, args);
+    }
+
+}
